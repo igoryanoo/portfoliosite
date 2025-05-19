e02911215896d1e3ef0bbf8fd93f7f55678fd44d
igoryanoo <igoryano248@gmail.com>
2025-05-19 00:05:01 -0300

    Atualizando links

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,8 +5,8 @@
     <p class="text-xl text-gray-300">Desenvolvedor Back-end</p>
           <div class="flex space-x-6 mb-10">
         <a href="https://www.linkedin.com/in/igor-yano-8624b5312/" class="text-white hover:text-blue-400 text-2xl"><i class="fab fa-linkedin"></i></a>
-        <a href="#" class="text-white hover:text-gray-300 text-2xl"><i class="fab fa-github"></i></a>
-        <a href="#" class="text-white hover:text-red-400 text-2xl"><i class="fas fa-envelope"></i></a>
+        <a href="https://github.com/igoryanoo" class="text-white hover:text-gray-300 text-2xl"><i class="fab fa-github"></i></a>
+        <a href="mailto:igoryano248@gmail.com" class="text-white hover:text-red-400 text-2xl"><i class="fas fa-envelope"></i></a>
       </div>
 </div>
 
